de01b71bd32f3985aef8a7eb369cd64fe208f64d
Vab Kapoor <vaib.kapoor15@gmail.com>
2026-01-20 17:54:14 -0500

    Created nyaa_utils for extracting relevant information from nyaa link like magnet url, etc. and cleaned up and formatted other files

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,13 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-from data_sources import initialize_media, refresh_episode_metadata, refresh_onepace_sheet 
+from data_sources import (
+    initialize_media,
+    refresh_episode_metadata,
+    refresh_onepace_sheet,
+)
 from metadata import build_episode_mapping, save_metadata_mapping
+from nyaa_utils import extract_nyaa_id, get_magnet_link, get_nyaa_resource_for_episode
 
 load_dotenv()
 
@@ -22,8 +27,16 @@ def calculate_crc32(filepath: str) -> str:
 if __name__ == "__main__":
     media_data_location = Path(os.getenv("MEDIA_DATA_LOCATION", "data/media"))
     initialize_media(media_data_location)
-    refresh_episode_metadata()
-    refresh_onepace_sheet()
     metadata_mapping = build_episode_mapping(media_data_location)
     print(f"Built metadata mapping for {len(metadata_mapping)} episodes.")
-    save_metadata_mapping(metadata_mapping, media_data_location)
+    # example_episode = metadata_mapping[0]
+    example_episode = metadata_mapping[0]
+    nyaa_resource = get_nyaa_resource_for_episode(example_episode)
+    print(get_magnet_link(nyaa_resource))
+    example_episode = metadata_mapping[33]
+    nyaa_resource = get_nyaa_resource_for_episode(example_episode)
+    print(get_magnet_link(nyaa_resource))
+    # print(example_episode)
+    # nyaa_id = extract_nyaa_id(example_episode)
+    # print(f"Example episode NyaaSi ID: {nyaa_id}")
+    # save_metadata_mapping(metadata_mapping, media_data_location)
