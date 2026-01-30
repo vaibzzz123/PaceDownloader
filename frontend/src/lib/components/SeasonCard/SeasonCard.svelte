1dd94dc4f69178c4cca3e06f6a2b8bb2c4e90fcb
Vab Kapoor <vaib.kapoor15@gmail.com>
2026-01-29 22:25:32 -0500

    Persisted state to localStorage, changed methods a bit so a blocking script runs to inject it asap, and added spoiler mode

--- frontend/src/lib/components/SeasonCard/SeasonCard.svelte
+++ frontend/src/lib/components/SeasonCard/SeasonCard.svelte
@@ -7,10 +7,13 @@
 <Card.Root class="gap-0 py-0 overflow-hidden w-fit px-0">
   <Card.Content class="p-0">
     <a href={`/season/${seasonNum}`}>
-      <img src={imagePath} alt={title} class="h-auto rounded-t-xl max-h-100" />
+      <img src={imagePath} alt={title} class="h-auto rounded-t-xl max-h-100 spoiler:blur-md" />
     </a>
   </Card.Content>
   <Card.Footer class="justify-center py-3 w-0 min-w-full">
-    <Card.Title class="text-2xl text-center text-wrap">{title}</Card.Title>
+    <Card.Title class="text-2xl text-center text-wrap">
+      <span class="spoiler:hidden">{title}</span>
+      <span class="hidden spoiler:inline">Season {seasonNum}</span>
+    </Card.Title>
   </Card.Footer>
 </Card.Root>
