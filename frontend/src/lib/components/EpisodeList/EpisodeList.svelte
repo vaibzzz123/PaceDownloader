c5befd22e5d6b6ed944f4ffbec4f1a7949dee302
Vab Kapoor <vaib.kapoor15@gmail.com>
2026-01-29 23:06:20 -0500

    Created basic table view for season episodes, added Table shadcn component

---
+++ frontend/src/lib/components/EpisodeList/EpisodeList.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import * as Table from "../ui/table";
+  let { episodes, season } = $props();
+</script>
+
+<Table.Root>
+  <Table.Header>
+    <Table.Row>
+      <Table.Head>Season</Table.Head>
+      <Table.Head>Episode</Table.Head>
+      <Table.Head>Name</Table.Head>
+      <Table.Head>Duration</Table.Head>
+      <Table.Head>Downloaded</Table.Head>
+    </Table.Row>
+  </Table.Header>
+  <Table.Body>
+    {#each episodes as episode}
+      <Table.Row>
+        <Table.Cell>{season}</Table.Cell>
+        <Table.Cell>{episode.number}</Table.Cell>
+        <Table.Cell>{episode.title}</Table.Cell>
+        <Table.Cell>{episode.duration}</Table.Cell>
+        <Table.Cell>{episode.downloaded ? 'Yes' : 'No'}</Table.Cell>
+      </Table.Row>
+    {/each}
+  </Table.Body>
+</Table.Root>
---
+++ frontend/src/routes/season/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { page } from '$app/state';
+  import EpisodeList from '$lib/components/EpisodeList/EpisodeList.svelte';
+
+  const pageData = {
+    imagePath: '../../../backend/data/eps-metadata/One Pace/Season 1/poster.png',
+    title: 'Romance Dawn',
+    episodes: [
+      { number: 1, title: 'Romance Dawn, the Dawn of an Adventure', duration: '24 min', downloaded: true },
+      { number: 2, title: 'They Call Him Straw Hat Luffy', duration: '22 min', downloaded: false },
+    ]
+  }
+</script>
+
+<EpisodeList episodes={pageData.episodes} season={page.params.id} />
