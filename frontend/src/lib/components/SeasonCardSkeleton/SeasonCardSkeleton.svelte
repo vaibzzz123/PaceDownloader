0e82bf3c089a76b86983964a92d48bcaa4d0ed63
Vab Kapoor <vaib.kapoor15@gmail.com>
2026-02-04 12:39:27 -0500

    Added Skeleton UI and created basic SeasonCardSkeleton component for testing

---
+++ frontend/src/lib/components/SeasonCardSkeleton/SeasonCardSkeleton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let { title, imagePath, seasonNum } = $props();
+</script>
+
+
+<div class="card p-4 preset-filled-primary-500">
+    <a href={`/season/${seasonNum}`}>
+      <img src={imagePath} alt={title} class="h-auto rounded-t-xl max-h-100 spoiler:blur-md" />
+    </a>
+    <div class="justify-center py-3 w-0 min-w-full text-2xl text-center text-wrap">
+      <span class="spoiler:hidden">{title}</span>
+      <span class="hidden spoiler:inline">Season {seasonNum}</span>
+    </div>
+</div>
