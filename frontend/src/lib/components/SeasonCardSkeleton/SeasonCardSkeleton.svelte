4d6ce9809eaa0b525b15de7b1f115475137f1688
Vab Kapoor <vaib.kapoor15@gmail.com>
2026-02-04 23:47:20 -0500

    Modified SeasonCard a bit to account for grid mode vs page mode, refactored some stuff to layout for consistency, working on SeasonInfo component

--- frontend/src/lib/components/SeasonCardSkeleton/SeasonCardSkeleton.svelte
+++ frontend/src/lib/components/SeasonCardSkeleton/SeasonCardSkeleton.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-  let { title, imagePath, seasonNum } = $props();
+  let { title, imagePath, seasonNum, gridMode } = $props();
 </script>
 
-<div class="card preset-filled-primary-500 transition-transform hover:scale-105">
-    <a href={`/season/${seasonNum}`} class="overflow-hidden block rounded-t-xl">
-      <img src={imagePath} alt={title} class="h-auto max-h-100 spoiler:blur-md spoiler:scale-110" />
-    </a>
-    <div class="justify-center py-3 w-0 min-w-full h5 text-center text-wrap">
-      <span class="spoiler:hidden">{title}</span>
-      <span class="hidden spoiler:inline">Season {seasonNum}</span>
-    </div>
+<div
+  class="card preset-filled-primary-500 {gridMode
+    ? 'transition-transform hover:scale-105'
+    : ''}"
+>
+  <a
+    href={`/season/${seasonNum}`}
+    class="overflow-hidden block rounded-t-xl {gridMode ? '' : 'pointer-events-none'}"
+  >
+    <img
+      src={imagePath}
+      alt={title}
+      class="h-auto {gridMode ? 'max-h-100' : 'max-h-100'} spoiler:blur-md spoiler:scale-110"
+    />
+  </a>
+  <div class="justify-center py-3 w-0 min-w-full h5 text-center text-wrap">
+    <span class="spoiler:hidden">{title}</span>
+    <span class="hidden spoiler:inline">Season {seasonNum}</span>
+  </div>
 </div>
--- frontend/src/routes/season/[id]/+page.svelte
+++ frontend/src/routes/season/[id]/+page.svelte
@@ -14,6 +14,11 @@
     imagePath: getPoster(page.params.id),
     title: 'Romance Dawn',
     description: 'Monkey D. Luffy sets out on an adventure to form a crew, find the legendary One Piece, and become the pirate king.',
+    // description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    // aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+    // dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+    // sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    // description: "The Straw Hats' next destination is Fishman Island, but first they must traverse the dreaded Florian Triangle which is known for many eerie and mysterious things such as disappearing crews, conversational skeletons, and a floating island full of zombies and other horrors.",
     episodes: [
       { number: 1, title: 'Romance Dawn, the Dawn of an Adventure', duration: '24 min', status: 'Hardlinked' },
       { number: 2, title: 'They Call Him Straw Hat Luffy', duration: '22 min', status: 'Copied' },
@@ -23,7 +28,7 @@
   }
 </script>
 
-<div class="flex flex-col gap-8">
+<div class="flex flex-col gap-20">
   <SeasonInfo seasonNum={page.params.id} title={pageData.title} imagePath={pageData.imagePath} description={pageData.description} />
   <EpisodeList episodes={pageData.episodes} season={page.params.id} />
 </div>
