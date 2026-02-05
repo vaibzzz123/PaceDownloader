a1b88263498d8f2a0ec12f6127da7fba2c1adcad
Vab Kapoor <vaib.kapoor15@gmail.com>
2026-02-04 23:15:19 -0500

    Added to Vite config data folder for easy access, modified components to account for it, and created new SeasonInfo component for season page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	resolve: {
+		alias: {
+			'$data': path.resolve('../backend/data')
+		}
+	},
 	server: {
 		fs: {
 			allow: ['..']
