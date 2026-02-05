f52216d199c7d7d26b0fd72bb9ef10925db65afc
Vab Kapoor <vaib.kapoor15@gmail.com>
2026-02-05 00:15:59 -0500

    Added Actions column in EpisodeList with corresponding lucide icon buttons

--- frontend/src/lib/components/EpisodeListSkeleton/EpisodeListSkeleton.svelte
+++ frontend/src/lib/components/EpisodeListSkeleton/EpisodeListSkeleton.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import SpoilerText from "../SpoilerText/SpoilerText.svelte";
+  import DownloadIcon from "@lucide/svelte/icons/download";
+  import Trash2Icon from "@lucide/svelte/icons/trash-2";
+  import PauseIcon from "@lucide/svelte/icons/pause";
+  import PlayIcon from "@lucide/svelte/icons/play";
   let { episodes, season } = $props();
 
   function statusStyle(status: string) {
@@ -10,6 +14,8 @@
     };
     return colors[status] ?? null;
   }
+
+  let isDownloadRunning = $state(false);
 
 </script>
 
@@ -22,6 +28,7 @@
         <th>Name</th>
         <th>Duration</th>
         <th>Status</th>
+        <th>Actions</th>
       </tr>
     </thead>
     <tbody>
@@ -35,6 +42,12 @@
           <td><SpoilerText>{episode.title}</SpoilerText></td>
           <td>{episode.duration}</td>
           <td>{episode.status}</td>
+          <td>
+            <!-- Temporary isDownloadRunning variable for previewing, will be dynamic once properly built -->
+            <button class="btn-icon" disabled={isDownloadRunning}><DownloadIcon /></button>
+            <button class="btn-icon" onclick={() => isDownloadRunning = !isDownloadRunning}>{#if isDownloadRunning}<PauseIcon/>{:else}<PlayIcon/>{/if}</button>
+            <button class="btn-icon"><Trash2Icon /></button>
+          </td>
         </tr>
       {/each}
     </tbody>
